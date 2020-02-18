43293f4254d65c58ae92bcdb4a97d9e25df81407
Canberk Sahin <canberksahin20@hotmail.com>
2020-02-18 16:07:13 +0300

    KafeVeri sınıfı eklendi

--- KafeKod.Data/KafeVeri.cs
+++ KafeKod.Data/KafeVeri.cs
@@ -8,5 +8,12 @@ namespace KafeKod.Data
 {
     class KafeVeri
     {
+        public List<Urun> Urunler { get; set; }
+
+        public List<Siparis> AktifSiparisler { get; set; }
+
+        public List<Siparis> GecmisSiparisler { get; set; }
+
+
     }
 }
